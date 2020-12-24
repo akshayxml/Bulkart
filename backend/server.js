588fc41d6527164362fe5b9389c6f8eef1a93d7b
Akshay M <akshayknr7@gmail.com>
2020-12-24 16:03:08 +0530

    pre-deployment

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,6 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
-
 app.use('/api/products', productRoutes) 
 app.use('/api/users', userRoutes) 
 app.use('/api/orders', orderRoutes) 
@@ -34,6 +30,18 @@ app.use('/api/upload', uploadRoutes)
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 /*Error handling middleware*/
 app.use(notFound)
