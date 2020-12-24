f74746c17dd202792b0119678e60b4836437f78b
Akshay M <akshayknr7@gmail.com>
2020-12-24 17:51:41 +0530

    update profile bug fix

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -16,6 +16,7 @@ import {
     USER_UPDATE_PROFILE_FAIL,
     USER_UPDATE_PROFILE_REQUEST,
     USER_UPDATE_PROFILE_SUCCESS,
+    USER_UPDATE_PROFILE_RESET,
     USER_DETAILS_RESET, 
     USER_DELETE_REQUEST,
     USER_DELETE_SUCCESS,
@@ -77,6 +78,8 @@ import {
         return { loading: false, success: true, userInfo: action.payload }
       case USER_UPDATE_PROFILE_FAIL:
         return { loading: false, error: action.payload }
+      case USER_UPDATE_PROFILE_RESET:
+        return {}
       default:
         return state
     }
